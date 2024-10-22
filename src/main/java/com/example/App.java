9496e1f04217089fc3fb02601e1a0c7eb11c4e8a
Yakantika roy <0902cs191065@rjit.ac.in>
2024-10-22 13:32:10 +0530

    Test webhook with ngrok

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,9 +3,13 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        // Adding a comment to test Jenkins integration
+        System.out.println("Hello Jenkins!");
+
     }
 
     public int add(int a, int b) {
         return a + b;
     }
+    
 }
